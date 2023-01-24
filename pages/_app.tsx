3a341fc2aad35c8051a92d735905994f48686b85
Shih-Yu Hwang <shihyuhwang@gmail.com>
2023-01-24 16:44:04 -0600

    added GTM code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import { getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/ra
 
 import { publicProvider } from 'wagmi/providers/public';
 import { infuraProvider } from 'wagmi/providers/infura';
+import Script from 'next/script';
 
 const { chains, provider, webSocketProvider } = configureChains(
   [ chain.polygon],
@@ -34,11 +35,21 @@ const client = createReactClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script id='google-tag-manager' strategy='afterInteractive'>
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_CODE}');
+      `}
+      </Script>
+
       <WagmiConfig client={wagmiClient}>
         <LivepeerConfig client={client}>
-            <RainbowKitProvider chains={chains} theme={darkTheme()}>
-              <Component {...pageProps} />
-            </RainbowKitProvider>
+          <RainbowKitProvider chains={chains} theme={darkTheme()}>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
         </LivepeerConfig>
       </WagmiConfig>
     </>
